45da9f7b0b249d09a450ad656bfa9c4ce4759f99
luchanos <lukash_91@mail.ru>
2021-01-11 19:40:07 +0300

    [upd] docker-compose file with postgres database. [upd] conftest database credentials.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ import asyncpg
 
 @pytest.fixture
 async def db_pool():
-    db_pool = await asyncpg.create_pool("0.0.0.0:8080/orders")
+    db_pool = await asyncpg.create_pool("postgres://postgres:dbpass@0.0.0.0:5432/db")
     yield db_pool
     await db_pool.close()
