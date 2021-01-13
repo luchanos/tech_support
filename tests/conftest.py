8d12ccd90a9bd81c87c9db25eda96b42a66ccaa9
luchanos <lukash_91@mail.ru>
2021-01-13 11:54:02 +0300

    [upd] infrastructure for creation test database.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,42 @@
 import pytest
 import asyncpg
+from subprocess import PIPE, Popen
+import os
+
+DB_ORDERS_TEST = "postgres://postgres:dbpass@0.0.0.0:5432/db"
 
 
-@pytest.fixture
+@pytest.fixture()
 async def db_pool():
-    db_pool = await asyncpg.create_pool("postgres://postgres:dbpass@0.0.0.0:5432/db")
+    db_pool = await asyncpg.create_pool(DB_ORDERS_TEST)
     yield db_pool
     await db_pool.close()
+
+
+def run_migrations(migrations_path):
+    cmd = f"yoyo apply --database {DB_ORDERS_TEST} {migrations_path} -b"
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    stdout, stderr = process.communicate()
+    if process.returncode:
+        raise Exception(f"Error during run migrations! {stderr} due to execution command '{cmd}'")
+
+
+def execute_sql_query(command):
+    cmd = f"psql -U postgres -h 0.0.0.0 -p 5432 -c '{command}' db"
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True, env=dict(os.environ, PGPASSWORD="dbpass"))
+    stdout, stderr = process.communicate()
+    if process.returncode:
+        raise Exception(f"Error during run sql query! {stderr} due to execution command '{cmd}'")
+
+
+@pytest.fixture(autouse=True, scope='session')
+def create_test_database():
+    execute_sql_query(
+        """
+        DROP SCHEMA IF EXISTS public CASCADE;
+        CREATE SCHEMA IF NOT EXISTS public;
+        GRANT USAGE ON SCHEMA public to PUBLIC;
+        GRANT CREATE ON SCHEMA public to PUBLIC;
+    """
+    )
+    run_migrations("./orders_db/migrations")
